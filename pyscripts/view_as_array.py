2383124efb16220fb786b2b2f81a02fb4a3b04b9
haochengxia <xhc_1007@163.com>
2024-03-22 17:10:07 +0800

    feat: initial scripts for arr and img view

---
+++ pyscripts/view_as_array.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+""" To enhance the visualization of NumPy array. 
+Ref: https://blog.jetbrains.com/pycharm/2014/11/third-pycharm-4-eap-numpy-array-viewer-ipython-notebook-improvements-and-more/
+"""
+
---
+++ pyscripts/view_as_image.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+""" To displays an OpenCV Image (ndarray or Mat)
+Ref: https://plugins.jetbrains.com/plugin/14371-opencv-image-viewer
+"""
